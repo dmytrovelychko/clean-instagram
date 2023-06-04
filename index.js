b8a3efab90e92dd16a74d2ad6f6a37e37ca46571
dmytrovelychko <dmytro.velychko.pro@gmail.com>
2023-06-04 23:38:31 +0300

    add notes

---
+++ index.js
@@ -0,0 +1,7 @@
+
+document.querySelectorAll('article') [0].parentElement.childNodes
+
+// find all articles tags
+// find div between articles that contain text: Suggested Posts, this is an indicator of dirt
+// make not visible all articles after this div tag
+
